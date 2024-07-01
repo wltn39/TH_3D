d5810ef574894bb2dcaa91953475cbc7569dd97c
gino <wltn39@gmail.com>
2024-07-01 11:48:11 +0900

    배경변경, 코인출현/파괴 알고리즘 변경

--- Assets/2. Scripts/1. TH/TH_Coin.cs
+++ Assets/2. Scripts/1. TH/TH_Coin.cs
@@ -6,20 +6,23 @@ using UnityEngine;
 
 public class TH_Coin : MonoBehaviour
 {
-    private float minY = -6f;
+    private float destroyTime = 5f; // 오브젝트가 사라질 시간 (초)
+
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody rigidBody = GetComponent<Rigidbody>();
-
+        Invoke("DestroyObject", destroyTime);
     }
 
-    // Update is called once per frame
+    // Update는 더 이상 필요하지 않습니다
     void Update()
     {
-        if (transform.position.y < minY)
-        {
-            Destroy(gameObject);
-        }
+    }
+
+    // 오브젝트를 파괴하는 메서드
+    void DestroyObject()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/2D_SD_Player/Scripts/TAnimation.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TAnimation : MonoBehaviour {
+
+    Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetRay();
+        MoveMouse();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (anim.GetBool("isAttack") == false)
+            {
+                anim.SetBool("isAttack", true);
+                anim.SetBool("isIdle", false);
+            }
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (anim.GetBool("isHited") == false)
+            {
+                anim.SetBool("isHited", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            if (anim.GetBool("isDie") == false)
+            {
+                anim.SetBool("isDie", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.F))
+        {
+            if (anim.GetBool("isRun") == false)
+            {
+                anim.SetBool("isRun", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else if (Input.GetKey(KeyCode.G))
+        {
+            if (anim.GetBool("isWalk") == false)
+            {
+                anim.SetBool("isWalk", true);
+                anim.SetBool("isIdle", false);
+            }
+        }
+        else
+        {
+            if (anim.GetBool("isIdle") == false)
+            {
+                anim.SetBool("isHited", false);
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isAttack", false);
+                anim.SetBool("isWalk", false);
+                anim.SetBool("isDie", false);
+                anim.SetBool("isRun", false);
+            }
+        }
+    }
+    
+
+        void MoveMouse()
+        {
+            //Gets the world position of the mouse on the screen        
+           // Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+       // transform.position = Vector3.Lerp(transform.position, mousePosition, Time.deltaTime);
+        }
+    RaycastHit hit;
+    public Transform GetRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.transform.tag == "Player")
+            {
+                return hit.transform;
+            }
+        }
+        return null;
+    }
+}
